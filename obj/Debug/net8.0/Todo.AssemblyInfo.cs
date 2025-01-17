610a4c63dd24aff5fc0f4cf35ea59889099c8180
Seikixo <150346850+Seikixo@users.noreply.github.com>
2025-01-17 15:53:09 +0800

    feat: Passing Data to Component using [Parameter]

--- obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a19d579b7508e098b2154113a5d0afa50d5e604")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b706ecb82937286d74b192432ee31c0ee5adbf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
